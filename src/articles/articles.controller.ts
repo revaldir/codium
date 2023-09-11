a26e8ad520aa43ba6e83d84bf8ca2fb11603a2db
Revaldi <revaldii32@gmail.com>
2023-09-11 19:03:40 +0700

    [IMP] - Basic Article CRUD with Prisma and Swagger implemented

---
+++ src/articles/articles.controller.ts
@@ -0,0 +1,66 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  ParseIntPipe,
+} from '@nestjs/common';
+import { ArticlesService } from './articles.service';
+import { CreateArticleDto } from './dto/create-article.dto';
+import { UpdateArticleDto } from './dto/update-article.dto';
+import { ApiCreatedResponse, ApiOkResponse, ApiTags } from '@nestjs/swagger';
+import { ArticleEntity } from './entities/article.entity';
+
+@Controller('articles')
+@ApiTags('articles')
+export class ArticlesController {
+  constructor(private readonly articlesService: ArticlesService) {}
+
+  // Create new article
+  @Post()
+  @ApiCreatedResponse({ type: ArticleEntity })
+  create(@Body() createArticleDto: CreateArticleDto) {
+    return this.articlesService.create(createArticleDto);
+  }
+
+  // Get unpublished article
+  @Get('drafts')
+  @ApiOkResponse({ type: ArticleEntity, isArray: true })
+  findDrafts() {
+    return this.articlesService.findDrafts();
+  }
+
+  // Get published article
+  @Get()
+  @ApiOkResponse({ type: ArticleEntity, isArray: true })
+  findAll() {
+    return this.articlesService.findAll();
+  }
+
+  // Find article by id
+  @Get(':id')
+  @ApiOkResponse({ type: ArticleEntity })
+  findOne(@Param('id', ParseIntPipe) id: number) {
+    return this.articlesService.findOne(id);
+  }
+
+  // Update article by id
+  @Patch(':id')
+  @ApiOkResponse({ type: ArticleEntity })
+  update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() updateArticleDto: UpdateArticleDto,
+  ) {
+    return this.articlesService.update(id, updateArticleDto);
+  }
+
+  // Remove article by id
+  @Delete(':id')
+  @ApiOkResponse({ type: ArticleEntity })
+  remove(@Param('id', ParseIntPipe) id: number) {
+    return this.articlesService.remove(id);
+  }
+}
---
+++ src/articles/dto/create-article.dto.ts
@@ -0,0 +1,18 @@
+import { ApiProperty } from '@nestjs/swagger';
+
+export class CreateArticleDto {
+  @ApiProperty()
+  title: string;
+
+  @ApiProperty()
+  slug: string;
+
+  @ApiProperty({ required: false })
+  description?: string;
+
+  @ApiProperty()
+  body: string;
+
+  @ApiProperty({ required: false, default: false })
+  published?: boolean = false;
+}
