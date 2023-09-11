3c7b0ce825574380ce0d2a99f8fb6b319ae5470e
Revaldi <revaldii32@gmail.com>
2023-09-11 19:16:16 +0700

    [IMP] - Implement pipe for validations

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Global validation pipe
+  /**
+   * option whitelist: ValidationPipe will automatically remove all non-whitelisted properties, where “non-whitelisted” means properties without any validation decorators.
+   */
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
 
   // Swagger setup
   const config = new DocumentBuilder()
